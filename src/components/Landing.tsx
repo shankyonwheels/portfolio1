e983e8d9e4081c7c89eab87d052c718a8972829b
shashank dwivedi <shashankdwivedi606.sd@gmail.com>
2026-04-20 14:50:10 +0530

    Update Landing.tsx
    update in landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -18,10 +18,10 @@ const Landing = ({ children }: PropsWithChildren) => {
             <h3 style={{ fontSize: "clamp(1.2rem, 3.5vw, 2rem)", fontWeight: 400, margin: 0, textTransform: "uppercase", letterSpacing: "2px" }}>
               Account Manager <br /> Lead IT Recruiter
             </h3>
-            <h2 style={{ fontSize: "clamp(1.5rem, 4vw, 2.5rem)", lineHeight: 1.2, margin: 0, textTransform: "none" }}>
+            <h2 style={{ fontSize: "clamp(1.2rem, 2vw, 2rem)", lineHeight: 1.2, margin: 0, textTransform: "none" }}>
               Global IT Recruitment
             </h2>
-            <h2 style={{ fontSize: "clamp(1.5rem, 4vw, 2.5rem)", lineHeight: 1.2, margin: 0, color: "var(--accentColor)", textTransform: "none" }}>
+            <h2 style={{ fontSize: "clamp(1.2rem, 2vw, 2rem)", lineHeight: 1.2, margin: 0, color: "var(--accentColor)", textTransform: "none" }}>
               Cybersecurity & Infra
             </h2>
           </div>
